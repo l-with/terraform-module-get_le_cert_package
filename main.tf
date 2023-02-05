d28256a38214bcaf741c731df0c2ad7b5375f7ac
Lars With <lars@with.de>
2023-02-05 20:34:10 +0100

    debug

--- main.tf
+++ main.tf
@@ -1,10 +1,18 @@
 data "system_command" "curl_le_cert_package" {
   command = "curl --output /tmp/le_${var.le_domains}.zip --header ${var.url_header} ${var.le_cert_package_url}"
+  expect {
+    stderr = true
+    stdout = true
+  }
 }
 
 data "system_command" "decrypt_and_untar_le_cert_package" {
   depends_on = [data.system_command.curl_le_cert_package]
   command    = "LE_CERT_PACKAGE_SECRET=${var.le_cert_package_secret} openssl enc -aes-256-cbc -pbkdf2 -d -pass env:LE_CERT_PACKAGE_SECRET -in /tmp/le_${var.le_domains}.zip | tar xzC /"
+  expect {
+    stderr = true
+    stdout = true
+  }
 }
 
 data "system_command" "when_finished" {
